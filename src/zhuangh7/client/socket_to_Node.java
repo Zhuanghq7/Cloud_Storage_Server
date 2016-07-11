a86062610a396db5244bcb1960f54cc5a7fe8757
Best_Logen <zhuanghq2007@foxmail.com>
2016-07-11 17:03:11 +0800

    2

--- src/zhuangh7/client/socket_to_Node.java
+++ src/zhuangh7/client/socket_to_Node.java
@@ -1,6 +1,8 @@
 package zhuangh7.client;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -32,24 +34,28 @@ public class socket_to_Node extends Thread{
 		JFrame jf = new JFrame("server");
 		jf.setSize(300, 200);
 		jf.setVisible(true);
+		jf.setLayout(new FlowLayout());
 		socket_to_Node stN = new socket_to_Node("1.txt");
 		Dimension screenSize =Toolkit.getDefaultToolkit().getScreenSize();
 		jf.setLocation(screenSize.width/2-150,screenSize.height/2-100);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		JPanel jp = new JPanel();
 	    jf.add (jp);
 	    JButton button = new JButton ("上传");
+	    
+	   // button.setSize(50, 100);
 	   //// button.setBounds (150-50,100-25,100,50);
 	    jf.add (button);
 	    button.addActionListener(new ActionListener(){
 	    	@Override
 	    	public void actionPerformed(ActionEvent e){
+	    		
 	    		stN.start();
 	    	}
 	    });
 		jf.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e) {
-				stN.Stop();//炸了炸了，礼拜一问老师- -
+				stN.Stop();//炸了炸了，礼拜一问老师- -//搞定……
 				System.exit(0);
 			}
 		});
@@ -61,6 +67,8 @@ public class socket_to_Node extends Thread{
 		try {
 			Socket s = new Socket("127.0.0.1",2016);
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(),"UTF-8"));//不管三七二十一咱先把这个文件名字写了是吧
+			bw.write("up\n");
+			bw.flush();
 			bw.write(filename+"\n");
 			bw.flush();
 			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream(),"UTF-8"));
@@ -82,14 +90,17 @@ public class socket_to_Node extends Thread{
 	                sumL += length;    
 	               // System.out.println("已传输："+((sumL/l)*100)+"%");  
 	                dos.write(sendBytes, 0, length);  
-	                dos.flush();  
+	                dos.flush(); 
+	                if(!stop){
+	    	            
+		            }
+		            else{   
+		            	System.out.println("传输终止");
+		            	break;
+		            }
 	            } 
-	            if(!stop){
+
 	            System.out.println("传输完成");
-	            }
-	            else{
-	            	System.out.println("传输终止");
-	            }
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
